404bc6d858993753b06232c2f980e962e4c76857
Chloe <gtalk.with.nani@gmail.com>
2024-01-11 16:00:50 +0900

    Update naverNews.ts

--- pages/api/naverNews.ts
+++ pages/api/naverNews.ts
@@ -1,32 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getNaverNewsData } from '@/utils/strapi';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const id = req.query.id as string;
+  if (req.method === 'GET') {
+    const start = Number(req.query.start) || 0;
+    const count = Number(req.query.count) || 20;
 
-  if (!id) {
-    return res.status(400).send('Please provide an id.');
-  }
-
-  try {
-    const response = await fetch(`${process.env.STRAPI_URL}/api/naver-news-productions/${id}`, {
-      headers: {
-        Authorization: `Bearer ${process.env.STRAPI_BEARER_TOKEN}`,
-      },
-    });
-    const articleData = await response.json();
-    const metaResponse = await fetch(
-      `https://naver-news-opengraph.vercel.app/api/og?url=${encodeURIComponent(
-        `https://n.news.naver.com/article/${articleData.data.attributes.oid}/${articleData.data.attributes.aid}`,
-      )}`,
-    );
-    const metaData = await metaResponse.json();
-    const mergedData = {
-      ...articleData.data,
-      metaData,
-    };
-
-    res.status(200).json(mergedData);
-  } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    const data = await getNaverNewsData(start, count);
+    res.status(200).json(data);
+  } else {
+    console.log('Unsupported method');
   }
 }
