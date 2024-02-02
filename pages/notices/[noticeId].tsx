65746fc2a32793c34bee21f9aae4d2cc80003e5b
Chloe <gtalk.with.nani@gmail.com>
2024-02-03 07:14:20 +0900

    Update [noticeId].tsx

--- pages/notices/[noticeId].tsx
+++ pages/notices/[noticeId].tsx
@@ -33,7 +33,7 @@ const Notice = ({ notice }: { notice: NoticeParalinkData | null }) => {
             pageTitles={`${notice.attributes.subject} - ${originTitle}`}
             pageTitle={`${notice.attributes.subject}`}
             pageDescription={`${notice.attributes.description}`}
-            pageImg={`https://nol2tr.dev1stud.io/og-image.webp?ts=${timestamp}`}
+            pageImg={`https://shorts.dev1stud.io/og-image.webp?ts=${timestamp}`}
           />
           <div className="top-link">
             <Anchor href="/notices">
