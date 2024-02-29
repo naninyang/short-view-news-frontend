da80c18bf72789ac5f52848c26a5d7f5d8ea52bd
Chloe <gtalk.with.nani@gmail.com>
2024-02-29 16:31:51 +0900

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,7 @@
 const CACHE_NAME = 'short-data';
 
 self.addEventListener('fetch', (event) => {
-  if (
-    event.request.url.includes('/api/articlesEntertainment') ||
-    event.request.url.includes('/api/articlesNews') ||
-    event.request.url.includes('/api/historyNaver') ||
-    event.request.url.includes('/api/historyYouTube') ||
-    event.request.url.includes('/api/pages') ||
-    event.request.url.includes('/api/periodtOmt') ||
-    event.request.url.includes('/api/periodtTimeline') ||
-    event.request.url.includes('/api/previews') ||
-    event.request.url.includes('/api/youtubeNews') ||
-    event.request.url.includes('/api/youtubePlaylist') ||
-    event.request.url.includes('/api/previews')
-  ) {
+  if (event.request.url.includes('/api/notices')) {
     event.respondWith(
       caches.open(CACHE_NAME).then((cache) => {
         return cache.match(event.request).then((response) => {
